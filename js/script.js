1bc734ffb797ee648daabd5c54e38d156082aee0
bpastega <www.bianca.pastega@gmail.com>
2023-10-05 21:58:18 -0300

    Added javascript code

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-open
+/*Função para abrir o menu de cores*/
+document.getElementById("OpenColors").onclick = 
+function OpenMenu() { 
+    document.getElementById("colorMenu").style.display = "initial"; 
+} 
+
+/*Função para fechar o menu de cores*/ 
+document.getElementById("x-symbol").onclick = 
+function CloseMenu() { 
+    document.getElementById("colorMenu").style.display = "none"; 
+} 
