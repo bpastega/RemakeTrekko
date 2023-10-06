16dbd78dd6d3934d412df76dbe99c8090b5c3896
bpastega <www.bianca.pastega@gmail.com>
2023-10-06 00:05:31 -0300

    Javascript

--- Bianca/js/script.js
+++ Bianca/js/script.js
@@ -1,13 +1,18 @@
 /*Função para abrir o menu de cores*/
 document.getElementById("OpenColors").onclick = 
+
 function OpenMenu() { 
     document.getElementById("colorMenu").style.display = "initial"; 
 } 
 
 /*Função para fechar o menu de cores*/ 
 document.getElementById("x-symbol").onclick = 
+
 function CloseMenu() { 
     document.getElementById("colorMenu").style.display = "none"; 
 } 
 
 /*Função para mudar a cor do fundo*/
+function ChangeBG(cor){
+    document.body.style.backgroundColor = cor;
+}
