ff7cbf66c419ba1d17e4198fb2b0c05de18dc398
tofullstack <caroleaveiro@gmail.com>
2023-10-11 00:21:18 -0300

    function saveTask att/working

--- Tofu/js/script.js
+++ Tofu/js/script.js
@@ -1,11 +1,11 @@
-document.getElementById("OpenColors").onclick = 
+// document.getElementById("OpenColors").onclick = 
 
 function OpenMenu() { 
     document.getElementById("colorMenu").style.display = "initial"; 
 } 
 
 /*Função para fechar o menu de cores*/ 
-document.getElementById("x-symbol").onclick = 
+// document.getElementById("x-symbol").onclick = 
 
 function CloseMenu() { 
     document.getElementById("colorMenu").style.display = "none"; 
@@ -21,7 +21,7 @@ function createTaskElement(taskName, taskDescription){
     const task = document.createElement('div');
     task.className = 'task';
     task.draggable = true;
-    task.id = `task-${Date-now()}`;
+    task.id = `task-${Date.now()}`;
 
     const taskNameElement = document.createElement('div');
     taskNameElement.className = 'task-name';
@@ -55,11 +55,23 @@ function addTask(columnId) {
         const taskContent = document.createElement("div");
         taskContent.className = "taskContent";
 
-        taskContent.appendChild(document.createTextNode(taskText)); // Nome da tarefa
-        taskContent.appendChild(document.createElement("hr")); // Quebra de linha
-        taskContent.appendChild(document.createTextNode(taskDescriptionIn)); // Descrição da tarefa
-        //console.log(taskContent)
-        saveTask();
+                    // METODO ANTERIOR A FUNÇAO SAVETASK
+        // taskContent.appendChild(document.createTextNode(taskText)); // Nome da tarefa
+        // taskContent.appendChild(document.createElement("hr")); // Quebra de linha
+        // taskContent.appendChild(document.createTextNode(taskDescriptionIn)); // Descrição da tarefa
+        // //function saveTask irá salvar as tarefas assim que adicionalas
+
+        const taskName = document.createElement("div");
+        taskName.className = "task-name";
+        taskName.appendChild(document.createTextNode(taskText));
+        taskContent.appendChild(taskName);
+        taskContent.appendChild(document.createElement("hr"));
+        //criando uma quebra de linha p ficar mais bonito
+        const taskDescription = document.createElement("div");
+        taskDescription.className = "task-description";
+        taskDescription.appendChild(document.createTextNode(taskDescriptionIn));
+        taskContent.appendChild(taskDescription);
+        // aqui esta adicionando as classes task-name e task-description que serão utilizadas no saveTask
 
         // excluir tarefa
         const deleteButton = document.createElement("button");
@@ -79,7 +91,7 @@ function addTask(columnId) {
         taskContent.appendChild(deleteButton);
         newTaskItem.appendChild(taskContent);
         document.getElementById(`${columnId}-task`).appendChild(newTaskItem);
-
+        saveTask();
 
         // newTaskItem.appendChild(document.createTextNode(taskText));  // why textnode?
         // document.getElementById(`${columnId}-task`).appendChild(newTaskItem);
@@ -96,27 +108,22 @@ function addTask(columnId) {
             tasks[columnId] = [];
             // console.log('===');
             console.log(columnId);
-            const taskElements = column.querySelectorAll('.task');
-
+            const taskElements = column.querySelectorAll('.taskItem');
+            //elementos de tarefas foram criados com a classe taskItem
 
             taskElements.forEach(task=>{
             const dataTask = {
                 name: task.querySelector('.task-name').innerText,
                 description: task.querySelector('.task-description').innerText
             };
-            tasks[columnId.push](dataTask);
+            tasks[columnId].push(dataTask);
         }); //fim do forEch tasks
 
         // console.log(dataTask );
         }); //fim do forEch column
 
 
-
-        // localStorage.setItem("tasks", JSON.stringify("column.id"));
-
-
-        // localStorage.setItem('todos', JSON.stringify("column.id"));
-
+        localStorage.setItem("tasks", JSON.stringify(tasks));
 
     } //fim da function saveTask
 // mudar tema
