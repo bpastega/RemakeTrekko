4ff77ad8186e7cfa2867441a5e38c8be4146fafb
bpastega <www.bianca.pastega@gmail.com>
2023-10-13 12:51:14 -0300

    Built addtask function base

--- Bianca2/js/script.js
+++ Bianca2/js/script.js
@@ -13,3 +13,50 @@ document.getElementById("x-symbol").onclick =
 function closeMenu() { 
     document.getElementById("colorMenu").style.display = "none"; 
 } 
+
+function addTask(columnId) {
+    // Get the title and description from the corresponding input fields
+    var titleInput = document.getElementById('title-' + columnId);
+    var descriptionInput = document.getElementById('desc-' + columnId);
+
+    var title = titleInput.value;
+    var description = descriptionInput.value;
+
+    // Do something with the title and description (e.g., create a new task)
+    console.log('Title:', title);
+    console.log('Description:', description);
+}
+
+/*
+function readTasks(){
+    const json = localStorage.getItem("tasks-data");
+    if(!json){
+        return[
+
+        ]
+
+    }
+}
+*/
+
+/*Função prof. Gustavo
+function loadTasks(){
+    const savedTasks = localStorage.getItem('tasks');
+    console.log(savedTasks);
+    if (savedTasks){
+        const tasks = JSON.parse(savedTasks);
+
+        Object.keys(tasks).forEach(columnId => {
+            const column = document.getElementById(columnId);
+            const columntasks = tasks(columnId);
+        });
+
+            columntasks.forEach(taskdata =>{
+                console.log(taskdata);
+                const task = createtaskelement(taskdata.name, taskdata.taskDescription);
+                column.querySelector('.tasks').appendChild(task);
+            }); 
+             
+    } 
+}
+*/
